f13ad13e3573ccebec8236037e4e20abb9abbac7
qr3n <qren.freelance@gmail.com>
2024-08-27 01:48:17 -0700

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
     dest: "public",
+    disable: true
 });
 
 export default withPWA({
--- src/shared/api/_api.ts
+++ src/shared/api/_api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { QueryClient } from "@tanstack/react-query";
 
 export const api = axios.create({
-    baseURL: 'https://postavan.com/api/driver',
+    baseURL: 'https://stubbybot.ru',
     timeout: 10000,
 })
 
