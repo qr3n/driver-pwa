0cd16a81b078973242bf59306eea1351d5162cf4
qr3n <qren.freelance@gmail.com>
2024-09-01 09:31:32 -0700

    Initial commit

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -12,7 +12,7 @@ export default async function DashboardPage() {
         <div className='flex flex-col w-full items-center justify-center pt-20 md:pt-24 px-4'>
             <UpdateOrdersWebsocket/>
             <h1 className='text-3xl sm:text-4xl font-semibold'>Все заказы</h1>
-            <Tabs defaultValue='today' className='mt-5 sm:mt-8 w-full flex flex-col items-center'>
+            <Tabs defaultValue='taked' className='mt-5 sm:mt-8 w-full flex flex-col items-center'>
                 <TabsList>
                     <TabsTrigger value='today' className='text-xs sm:text-sm'>На сегодня</TabsTrigger>
                     <TabsTrigger value='planned' className='text-xs sm:text-sm'>Запланированные</TabsTrigger>
