b93fe5f9268a96d1a023154a5a81921b4bfb23a3
qr3n <qren.freelance@gmail.com>
2024-09-10 05:27:29 -0700

    Initial commit

--- src/app/orders/layout.tsx
+++ src/app/orders/layout.tsx
@@ -1,7 +1,14 @@
 import { PropsWithChildren, Suspense } from "react";
 import Loading from "@/app/orders/loading";
+import { useServerSession } from "@/entities/session/server";
+import { redirect } from "next/navigation";
 
 export default function OrdersLayout({ children }: PropsWithChildren) {
+    const session = useServerSession()
+
+    if (!session) redirect('/auth/login')
+
+
     return (
         <Suspense fallback={<Loading/>}>
             { children }
